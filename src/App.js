9c5415efc734969fcc5c24711c4ba65ebbf99070
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-29 12:26:24 -0600

    scss styles, apollo cliente configured

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Button } from 'semantic-ui-react'
+import { ApolloProvider } from '@apollo/client'
+import client from './config/apollo'
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>
-        APP
-      </h1>
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>
+          APP
+        </h1>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
   );
 }
 
